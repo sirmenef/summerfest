78ca0cd95b4d4d3ddffa4d68e4cbf3fc6d9957e8
Sirmenef <wearysamm@gmail.com>
2020-06-30 16:15:12 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from './services/api.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+// import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'summerFest';
+export class AppComponent implements OnInit{
+
+  form: FormGroup;
+  isSubmitted: boolean;
+
+  constructor(private api: ApiService, private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      childName: ['', Validators.required],
+      gender: ['', Validators.required],
+      age: ['', Validators.required],
+      gradeInSession: ['', [Validators.required]],
+      guardianContact: ['', Validators.required],
+      email: ['', Validators.required],
+      homeCity: ['', Validators.required],
+      childSchool: ['', Validators.required],
+      activityTime: ['', Validators.required],
+      date: ['', Validators.required],
+      terms: [false, Validators.required],
+    });
+    // this.form.controls.gender.setValue('Select');
+  }
+
+  onSubmit() {
+    this.form.controls['date'].setValue(new Date().toDateString());
+    if (this.form.invalid) { return; }
+    // this.isSubmitted = true;
+    this.api.submitForm(this.form.value);
+  }
+
+  get controls() { return this.form.controls; }
+
+  terms() {
+    this.form.controls['terms'].setValue(true);
+  }
+  onReset() {
+    this.isSubmitted = false;
+    // this.form.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+      ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
